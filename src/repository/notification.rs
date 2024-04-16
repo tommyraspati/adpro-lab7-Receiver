6fbc663292d433c13af9eca1266845058c11e801
raspatimahatma <tommymahatma08@gmail.com>
2024-04-16 22:15:19 +0700

    Implement list_all_as_string function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -14,5 +14,7 @@ impl NotificationRepository{
         NOTIFICATIONS.write().unwrap().push(notification.clone());
         return notification;
     }
-
+    pub fn list_all_as_string()-> Vec<String>{
+        return NOTIFICATIONS.read().unwrap().iter().map(|f| format!("{}",f.clone())).collect();
+    }
 }
