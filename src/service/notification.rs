0fc60c4fd74d78dd5714cac3d29346a5dfbb939d
raspatimahatma <tommymahatma08@gmail.com>
2024-04-16 22:39:42 +0700

    Implement list_messages function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -80,4 +80,8 @@ impl NotificationService{
         return Ok(subscriber_result);
     }
 
+    pub fn list_message()-> Result<Vec<String>>{
+        return Ok(NotificationRepository::list_all_as_string());
+    }
+
 }
